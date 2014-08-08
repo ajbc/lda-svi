c5c880b72316bfe67f8de537c654ecd2b1f73846
ajbc <ajbchaney@gmail.com>
2014-08-08 09:55:37 -0400

    added lemmatization to vocab curation (still need to add it to live parse)

--- curate_vocab.py
+++ curate_vocab.py
@@ -1,9 +1,10 @@
 # TODO: add lemmitization
 
-import argparse, os, fnmatch
+import argparse, os, fnmatch, re
 from collections import defaultdict
 import numpy as np
 from bs4 import BeautifulSoup
+import lemmatize
 
 parser = argparse.ArgumentParser(description = \
     'curate a vocabulary .')
@@ -13,14 +14,16 @@ parser.add_argument('doc_path', metavar='doc_path', type=str, \
 parser.add_argument('--out', metavar='out', type=str, \
     default='vocab.dat', help = 'output file')
 
-parser.add_argument('--min_doc_count', metavar='min_doc_count', type=int, \
+parser.add_argument('--min_doc_count', dest='min_doc_count', type=int, \
     default=10, help = 'the minimum # of documents a word must be in')
-parser.add_argument('--max_doc_percent', metavar='max_doc_per', type=float, \
+parser.add_argument('--max_doc_percent', dest='max_doc_per', type=float, \
     default=30, help = 'the maximum % of documents a word can be in')
-parser.add_argument('--min_tfidf', metavar='min_tfidf', type=float, \
+parser.add_argument('--min_tfidf', dest='min_tfidf', type=float, \
     default=np.exp(8), help = 'the minimumm tfidf for a word')
 #TODO add log tfidf option (same as above, but in log form), and make them
 # mutually exclusive
+parser.add_argument('--lem', dest='lem', action='store_true', \
+    default=False, help='lemmatize vocab (LDA should then use lemmatize arg).')
 
 args = parser.parse_args()
 
@@ -47,10 +50,12 @@ for (root,dirnames,filenames) in os.walk(args.doc_path):
         all = re.sub(r'-', ' ', all)
         all = re.sub(r'[^a-z ]', '', all)
         all = re.sub(r' +', ' ', all)
-        words = string.split(all)
+        words = all.split()
 
         found = set()
         for word in words:
+            if args.lem:
+                word = lemmatize.lem(word)
             if len(word) < 3:
                 continue
             if word not in found:
